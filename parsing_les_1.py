df3fdb11bcb95699054fa6e7e8636dcf33450182
agamora <agamora74@gmail.com>
2019-12-20 15:52:28 +0300

    Update parsing_les_1.py

--- parsing_les_1.py
+++ parsing_les_1.py
@@ -15,3 +15,21 @@ if response.ok:
     for i in data:
         repos.append(i['name'])
     print(repos)
+
+
+from pprint import pprint
+import requests
+import json
+import time
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
+
+main_link = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json & ' \
+            'AIzaSyB7SNmXXbNEe81dmQ77_bbRa1e0lsKL-c0parameters & London'
+
+
+response = requests.get(main_link)
+
+if response.ok:
+    time.sleep(1)
+    data = json.loads(response.text)
+    pprint(data)
